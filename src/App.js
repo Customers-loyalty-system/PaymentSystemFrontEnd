2b98ef400392a178d32338c03aaf962195952bc8
ubaeida <ubaeida.alkayal@gmail.com>
2023-01-24 12:26:26 +0300

    Payment System frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import "./App.css";
 
-function App() {
+import Loading from "./components/Loading/Loading";
+const Register = React.lazy(() => import("./pages/Register/Register"));
+const Login = React.lazy(() => import("./pages/Login/Login"));
+// const Logout = React.lazy(() => import("./pages/Logout/Logout"));
+const Payment = React.lazy(() => import("./components/Payment/Payment"));
+
+const App = () => {
+  const navigate = useNavigate()
+  const token = localStorage.getItem("token");
+  useEffect( ()=>  { 
+    navigate('/')
+    // eslint-disable-next-line
+  }, [token])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            token ? (
+              <Suspense fallback={<Loading />}>
+                <Payment />
+              </Suspense>
+            ) : (
+              <Suspense fallback={<Loading />}>
+                <Login />
+              </Suspense>
+            )
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Register />
+            </Suspense>
+          }
+        />
+      </Routes>
+      ;
+    </>
   );
-}
+};
 
 export default App;

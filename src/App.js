c7f646adb32cd348771235ac9cec0e8b9f2801ae
Yamen Aless <yamenaless@gmail.com>
2023-02-03 13:19:28 +0300

    Navigation has been fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Suspense  } from "react";
-import { Route, Routes  } from "react-router-dom";
+import React, { Suspense, useEffect } from "react";
+import { Route, Routes, useNavigate  } from "react-router-dom";
 import "./App.css";
 
 import Loading from "./components/Loading/Loading";
@@ -11,23 +11,26 @@ const Payment = React.lazy(() => import("./components/Payment/Payment"));
 
 const App = () => {
   const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) navigate("/login");
+    else return
+    // eslint-disable-next-line
+  }, []);
   return (
     <>
       <Routes>
-        <Route
+        {token &&
+          <Route
           path="/"
           element={
-            token ? (
-              <Suspense fallback={<Loading />}>
-                <Payment />
-              </Suspense>
-            ) : (
-              <Suspense fallback={<Loading />}>
-                <Login />
-              </Suspense>
-            )
+            <Suspense fallback={<Loading />}>
+              <Payment />
+            </Suspense>
           }
         />
+        }
         <Route
           path="/register"
           element={
